8f32c832af7598aaa9270e6e72d470bca8f78fc7
drew igoe <drewigoe@gmail.com>
2026-02-06 00:08:09 -0700

    Refactor NavBar on HomePage

--- src/assets/components/HomePage/HomePage.jsx
+++ src/assets/components/HomePage/HomePage.jsx
@@ -3,7 +3,7 @@ import NavBar from "../NavBar/NavBar.jsx";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col gap-2 bg-[rgb(249,249,249)]">
       <NavBar />
     </div>
   );
