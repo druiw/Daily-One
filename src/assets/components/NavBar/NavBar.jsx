46653627b5c8a9bbaaedee98d54abac618e807c3
Drew Igoe <drewigoe@gmail.com>
2026-02-06 22:59:50 -0700

    Update NavBar.jsx

--- src/assets/components/NavBar/NavBar.jsx
+++ src/assets/components/NavBar/NavBar.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { Menu } from "lucide-react";
 
 const NavBar = () => {
+  const handleMenuClick = async (e) => {
+    alert("Hi");
+  };
+
   return (
-    <nav className="flex align-middle justify-center p-5 bg-white">
+    <nav className="flex flex-row align-middle justify-center shadow gap-10 p-5 bg-white">
       One Question a Day
+      <button onClick={handleMenuClick} className="cursor-pointer">
+        <Menu className="h-6 w-6 text-gray-500" />
+      </button>
     </nav>
   );
 };
