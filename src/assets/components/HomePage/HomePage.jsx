1595894a001f678b128d938e8c1271eef759c697
Drew Igoe <drewigoe@gmail.com>
2026-02-09 16:54:55 -0700

    Moved prompt fetching to its own component.

--- src/assets/components/HomePage/HomePage.jsx
+++ src/assets/components/HomePage/HomePage.jsx
@@ -1,34 +1,12 @@
 import React from "react";
-import { useEffect, useState } from "react";
 import NavBar from "../NavBar/NavBar.jsx";
+import DailyQuestion from "../DailyQuestion/DailyQuestion.jsx";
 
 const HomePage = () => {
-  const url = "http://localhost:5000/api/prompts";
-
-  // Get all prompts from database and store in an array called prompts
-  useEffect(() => {
-    async function getPrompts() {
-      try {
-        const response = await fetch(url);
-
-        if (!response.ok) {
-          console.log(`Could not fetch resource.`);
-        }
-
-        const data = await response.json();
-        const prompts = data.map((p) => p.text);
-        console.log(prompts);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    getPrompts();
-  }, []);
-
   return (
     <div className="min-h-screen flex flex-col gap-2 bg-[rgb(249,249,249)]">
       <NavBar />
+      <DailyQuestion />
     </div>
   );
 };
