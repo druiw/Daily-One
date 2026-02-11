9d789c6538b7fdd2822b329ef35cdd667e8b56e8
Drew Igoe <drewigoe@gmail.com>
2026-02-10 20:21:23 -0700

    Update DailyQuestion.jsx

--- src/assets/components/DailyQuestion/DailyQuestion.jsx
+++ src/assets/components/DailyQuestion/DailyQuestion.jsx
@@ -38,8 +38,9 @@ const DailyQuestion = () => {
   }, []);
 
   return (
-    <div className="flex text-center justify-center gap-5 w-full max-w-md mx-auto bg-white rounded-2xl shadow p-8">
-      {prompt}
+    <div className="flex flex-col text-center md:text-left justify-center gap-5 w-full max-w-xl mx-auto bg-green-200 opacity-85 rounded-2xl shadow p-8">
+      <p className="font-medium text-sm text-green-600">TODAY'S QUESTION</p>
+      <div className="text-3xl ">{prompt}</div>
     </div>
   );
 };
