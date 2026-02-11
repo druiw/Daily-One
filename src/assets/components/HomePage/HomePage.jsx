c6245c14af1a77bb92f1dece56db640ca6b33148
Drew Igoe <drewigoe@gmail.com>
2026-02-10 20:21:40 -0700

    Tweaked HomePage.jsx

--- src/assets/components/HomePage/HomePage.jsx
+++ src/assets/components/HomePage/HomePage.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { useState } from "react";
 import NavBar from "../NavBar/NavBar.jsx";
 import DailyQuestion from "../DailyQuestion/DailyQuestion.jsx";
+import UserEntry from "../UserEntry/UserEntry.jsx";
+import SaveEntry from "../SaveEntry/SaveEntry.jsx";
 
 const HomePage = () => {
+  const [entry, setEntry] = useState("");
+  const handleSave = () => {
+    if (entry.trim()) {
+      alert("Entry Saved");
+      setEntry("");
+    }
+  };
   return (
-    <div className="min-h-screen flex flex-col gap-2 bg-[rgb(249,249,249)]">
+    <div className="min-h-screen flex flex-col gap-6 bg-[rgb(249,249,249)]">
       <NavBar />
       <DailyQuestion />
+      <UserEntry entry={entry} setEntry={setEntry} />
+      <SaveEntry onSave={handleSave} />
     </div>
   );
 };
